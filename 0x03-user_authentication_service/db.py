1faed0ece08d5d526ec4ee516346cf14cff2a6c7
SIMOHAMMED22 <simofinance22@gmail.com>
2024-04-23 16:26:04 +0100

    db 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
 """ module for db """
-
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
-from user import User, Base
+from user import Base, User
 
 
 class DB:
-    def __init__(self):
-        """
-        Initializes the class instance.
+    """ Class for DB """
 
-        Parameters:
-            None
+    def __init__(self) -> None:
+        """ Constructor """
+        self._engine = create_engine("sqlite:///a.db",
+                                     echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
 
-        Returns:
-            None
-        """
-        engine = create_engine('sqlite:///mydatabase.db')
-        Base.metadata.create_all(engine)
-        self._session = sessionmaker(bind=engine)()
+    @property
+    def _session(self) -> Session:
+        """ Method that creates a session if not exists """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
+        """ Add a user to the database """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
