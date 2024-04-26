0fa5b27a5f0c193934c265cb421bbafb2e71395f
SIMOHAMMED22 <simofinance22@gmail.com>
2024-04-26 16:33:11 +0100

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -111,7 +111,7 @@ class Auth:
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
 
-    def update_password(self, reset_token, password):
+    def update_password(self, reset_token: str, password: str) -> None:
         """
         Update the password for a user using a reset token.
 
