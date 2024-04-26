515efce347eeb42712817544d064aa59262b0357
SIMOHAMMED22 <simofinance22@gmail.com>
2024-04-26 15:38:13 +0100

    app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+import dbm
+from os import abort
+import uuid
+import dbus
+from flask import Flask, jsonify, make_response, request
 from flask.json import jsonify as flask_jsonify
+from mysqlx import DbDoc
 from werkzeug.exceptions import BadRequest
+from sqlalchemy.orm.exc import NoResultFound
 
 from auth import Auth
+from db import DB
+from user import User
 
 app = Flask(__name__)
 
@@ -51,3 +59,29 @@ def users():
 
     # Return a JSON payload with a success message and the registered email
     return flask_jsonify({"email": user.email, "message": "user created"})
+
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+        """Handle requests to get a reset password token.
+
+        :return: A JSON payload with the user's email and reset token if the email is registered, or a 403 HTTP status if the email is not registered.
+        :rtype: flask.Response
+        """
+        email = request.form.get("email")
+
+        if email is None:
+            abort(403, description="Email is required")
+
+        try:
+            user = dbus._session.query(User).filter_by(email=email).first()
+        except NoResultFound:
+            abort(403, description="Email is not registered")
+
+        reset_token = str(uuid.uuid4())
+        user.reset_token = reset_token
+        DbDoc._session.commit()
+
+        response = make_response(jsonify({"email": user.email, "reset_token": reset_token}))
+        response.status_code = 200
+
+        return response
